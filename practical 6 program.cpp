7e2b5d4f87c380017ece28f7df2cf7f038c65ef3
muthangowda9-sketch <muthangowda9@gmail.com>
2026-09-09 09:38:58 +0530

    Update practical 6 program.cpp

--- practical 6 program.cpp
+++ practical 6 program.cpp
@@ -1,23 +1,9 @@
 #include <iostream>
+#include <chrono>
 using namespace std;
 
-int main()
+int matrixChainMultiplication(int p[], int n)
 {
-    int n;
-
-    cout << "Enter number of matrices: ";
-    cin >> n;
-
-    int p[n + 1];
-
-    cout << "Enter the dimensions of matrices:" << endl;
-    cout << "For example, for matrices 10x20, 20x30, 30x40 enter: 10 20 30 40" << endl;
-
-    for (int i = 0; i <= n; i++)
-    {
-        cin >> p[i];
-    }
-
     int dp[n + 1][n + 1];
 
     // Cost of multiplying one matrix is 0
@@ -26,7 +12,7 @@ int main()
         dp[i][i] = 0;
     }
 
-    // length is the number of matrices in the chain
+    // length = number of matrices in the chain
     for (int length = 2; length <= n; length++)
     {
         for (int i = 1; i <= n - length + 1; i++)
@@ -35,9 +21,11 @@ int main()
 
             dp[i][j] = 999999999;
 
+            // Try every possible split
             for (int k = i; k < j; k++)
             {
-                int cost = dp[i][k] + dp[k + 1][j]
+                int cost = dp[i][k]
+                         + dp[k + 1][j]
                          + p[i - 1] * p[k] * p[j];
 
                 if (cost < dp[i][j])
@@ -48,8 +36,41 @@ int main()
         }
     }
 
-    cout << "\nMinimum number of multiplications = ";
-    cout << dp[1][n] << endl;
+    return dp[1][n];
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter number of matrices: ";
+    cin >> n;
+
+    int p[n + 1];
+
+    cout << "Enter dimensions: ";
+    for (int i = 0; i <= n; i++)
+    {
+        cin >> p[i];
+    }
+
+    // Start timer
+    auto start = chrono::high_resolution_clock::now();
+
+    // Run Matrix Chain Multiplication
+    int result = matrixChainMultiplication(p, n);
+
+    // End timer
+    auto end = chrono::high_resolution_clock::now();
+
+    // Calculate execution time
+    auto time = chrono::duration_cast<chrono::nanoseconds>(end - start);
+
+    cout << "\nMinimum number of multiplications = "
+         << result << endl;
+
+    cout << "Execution Time = "
+         << time.count() << " ns" << endl;
 
     return 0;
 }
